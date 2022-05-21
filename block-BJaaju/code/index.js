4ffc637375a83a05e4df2e42d6d5277fb043b3bc
dhairyasheel <dhairyasheeln@gmail.com>
2022-05-21 16:41:23 +0530

    Appliction Complete

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -1,1 +1,42 @@
-let a=0;
+const url='https://api.unsplash.com/photos/?client_id=4I2MgsxMZFn14w9cmNiVO2AS0ml4gDJjoggwUAsn7UU';
+const searchURL=(query)=>`https://api.unsplash.com/search/photos?query=${query}&client_id=4I2MgsxMZFn14w9cmNiVO2AS0ml4gDJjoggwUAsn7UU`;
+
+const root=document.querySelector('.images');
+const search=document.getElementById('search');
+
+
+function fetch(url,successHandler){
+    let xhr=new XMLHttpRequest();
+    xhr.open('GET',url);
+    xhr.onload=function(){
+        successHandler(JSON.parse(xhr.response));
+    };
+    xhr.onerror=function(){
+        console.error('Something went wrong!');
+    };
+    xhr.send();
+}
+
+function displayImages(images){
+    root.innerHTML="";
+    images.forEach(image => {
+        let li=document.createElement('li');
+        let img=document.createElement('img');
+        img.src=image.urls.thumb;
+        li.append(img);
+        root.append(li);
+    });
+}
+
+fetch(url,displayImages);
+
+search.addEventListener('keydown',handleSearch);
+
+function handleSearch(event){
+    if(event.keyCode==13 && search.value){
+        fetch(searchURL(search.value),function(searchResult){
+                displayImages(searchResult.results);
+        });
+        search.value="";
+    }
+}
